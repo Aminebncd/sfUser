30e8e814837a85eedc149a952951630c819efebf
aminebncd <amine.bounachada@hotmail.com>
2024-03-04 10:42:16 +0100

    added redirections

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,8 +20,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
             ->add('pseudo', TextType::class)
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
